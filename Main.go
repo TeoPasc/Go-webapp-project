46406da3075097b04b3f88dcec1d5ac4511ab62e
TeoPasc <teopascalev@gmail.com>
2024-12-27 15:00:47 +0200

    Update Main.go

--- Main.go
+++ Main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+        "fmt"
+        "log"
+        "net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello from Mondoo Engineer!\n")
-    })
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Fprintf(w, "Hello from Mondoo Engineer!\n")
+        })
 
-    fmt.Println("Server listening on :8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        panic(err)
-    }
+        log.Println("Server listening on :8080") 
+        err := http.ListenAndServe(":8080", nil)
+        if err != nil {
+                log.Fatal(err) 
+        }
 }
